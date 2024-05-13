dab52ad3880818d9baf21da4c42c01d1e3ec5f7d
philip.calape <philip.calape@whitecloak.com>
2024-05-13 21:45:53 +0800

    Create dummy data

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,10 @@ import ArticleList from './components/ArticleList';
 import './App.css';
 
 function App() {
-  const [articles, setArticles] = useState([]);
+  const [articles, setArticles] = useState([
+    { id: 1, title: 'What is Lorem Ipsum', content: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', createdDate: '2024-05-13'}, 
+    { id: 2, title: 'Another Article', content: 'Another Content', createdDate: '2024-05-13' }
+  ]);
   const [selectedArticle, setSelectedArticle] = useState(null);
 
   // This state is used to determine which screen to display
